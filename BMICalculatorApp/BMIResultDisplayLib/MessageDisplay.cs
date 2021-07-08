f6b545b5dd92d3f5ee30f8348279d9d9b078abae
Ubed Patait <67995958+ubedullahpatait@users.noreply.github.com>
2021-07-08 16:42:36 +0530

    updated

---
+++ BMICalculatorApp/BMIResultDisplayLib/MessageDisplay.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMIResultDisplayLib
+{
+    public class MessageDisplay
+    {
+
+        public void Display(string message)
+        {
+
+            Console.WriteLine(message);
+        }
+
+    }
+}
---
+++ BMICalculatorApp/BMIValueValidationLib/BmiValueValidation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMIValueValidationLib
+{
+    public class BmiValueValidation
+    {
+        public string CheckBmiValue(double BMIValue)
+        {
+            if (BMIValue < 18.5)
+            {
+                return "UnderWeight";
+            }
+            else if (BMIValue >= 25)
+            {
+                return "OverWeight";
+            }
+            else
+            {
+                return "Normal";
+            }
+        }
+
+    }
+}
