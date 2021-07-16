ddb00f3cc110d4e1e13f60309d3a97f3ff29b500
Ubed Patait <67995958+ubedullahpatait@users.noreply.github.com>
2021-07-16 14:44:16 +0530

    Add files via upload

---
+++ TATACar/TATACar/RevorTron.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TATACar
+{
+    class RevorTron:IEngine
+    {
+        public void Start()
+        {
+            Console.WriteLine("start");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("stop");
+        }
+
+
+
+    }
+}
